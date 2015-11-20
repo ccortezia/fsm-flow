f42b42ebabb1e4e6b41132a927d1754c80602f92
Cristiano Cortezia <cristiano.cortezia@gmail.com>
2015-11-20 18:44:26 -0200

    feat(fsm): add duplex stream wrapper on the main fsm
    Should allow the idiom events.pipe(fsm).pipe(states).
    Now this code needs some refactoring.
    
    One option is to improve this wrapping.
    
    Another option is to change FSM from having inner streams
    to being a duplex stream itself.

--- test/fsm-stream.spec.js
+++ test/fsm-stream.spec.js
@@ -1,4 +1,5 @@
 FSM = require('../lib/fsm').FSM;
+FSMFlow = require('../lib/fsm').FSMFlow;
 
 
 describe('An non-empty valid fsm generator stream', function(){
@@ -101,4 +102,58 @@ describe('An non-empty valid fsm generator stream', function(){
     eventSource.pipe(fsm.istream);
     fsm.ostream.pipe(stateDrain);
   });
+});
+
+
+
+describe('An non-empty valid flow fsm', function(){
+  var fsm, acc;
+
+  var map = {
+    'st0': {'ev0': null, 'ev1': 'st1', 'ev2': 'st2'},
+    'st1': {'ev0': null, 'ev1': null, 'ev3': 'st3'},
+    'st2': {'ev0': null, 'ev1': 'st1'},
+    'st3': {'ev0': null, 'ev1': 'st1'},
+  };
+
+  beforeEach(function(){
+    fsm = new FSMFlow(map, 'st0');
+    fsm.init();
+    acc = [];
+  });
+
+  it('should work in a pipe', function(done){
+    var events = ['ev1', 'ev3', 'ev1', 'ev3', 'ev1', 'ev3', 'ev0', null];
+    var states = ['st1', 'st3', 'st1', 'st3', 'st1', 'st3'];
+
+    var eventSource = new require('stream').Readable({
+      read: function(n) {
+        this.push(events.shift());
+      }
+    });
+
+    var stateDrain = new require('stream').Duplex({
+      read: function() {
+        if (acc.length >= states.length) {
+          for (var st of states) this.push(st + '\n');
+          states.length = 0;
+        }
+      },
+      write: function(chunk, encoding,  callback) {
+        chunk = chunk.toString('ascii');
+        acc.push(chunk);
+        if (acc.length >= states.length) {
+          expect(acc).toEqual(states);
+          expect(fsm.done()).toBe(true);
+          done();
+        }
+        callback();
+      }
+    });
+
+    eventSource
+      .pipe(fsm)
+      .pipe(stateDrain);
+  });
+
 });
